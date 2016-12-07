a69c13544bd888334e15d7fccb3caea3d58791f7
Bruno Henriques <bruno.s.henriques@tecnico.ulisboa.pt>
2016-12-07 15:40:23 +0000

    Optimize images

--- res/scripts.js
+++ res/scripts.js
@@ -9,22 +9,22 @@ let gamers = [
 ];
 
 let games = [
-	{ name: "League of Legends", img: "league.png" },
-	{ name: "Minecraft", img: "minecraft.png" },
-	{ name: "Counter-Strike", img: "csgo.png" },
-	{ name: "Team Fortress 2", img: "tf2.png" },
-	{ name: "Left 4 Dead 2", img: "left4dead2.png" },
-	{ name: "Killing Floor", img: "killingfloor.png" },
-	{ name: "DayZ", img: "dayz.png" },
-	{ name: "Age of Empires II", img: "aoe2.png" },
-	{ name: "PayDay 2", img: "payday2.png" },
-	{ name: "Just Cause 2", img: "justcause2.png" },
-	{ name: "Civilization 5", img: "civilization5.png" },
-	{ name: "TERA Europe", img: "tera.png" },
-	{ name: "Runescape", img: "runescape.png" },
-	{ name: "Spiral Knights", img: "spiralknights.png" },
-	{ name: "Metin2", img: "metin2.png" },
-	{ name: "Teeworlds", img: "teeworlds.png" }
+	{ name: "League of Legends", img: "league-min.png" },
+	{ name: "Minecraft", img: "minecraft-min.png" },
+	{ name: "Counter-Strike", img: "csgo-min.png" },
+	{ name: "Team Fortress 2", img: "tf2-min.png" },
+	{ name: "Left 4 Dead 2", img: "left4dead2-min.png" },
+	{ name: "Killing Floor", img: "killingfloor-min.png" },
+	{ name: "DayZ", img: "dayz-min.png" },
+	{ name: "Age of Empires II", img: "aoe2-min.png" },
+	{ name: "PayDay 2", img: "payday2-min.png" },
+	{ name: "Just Cause 2", img: "justcause2-min.png" },
+	{ name: "Civilization 5", img: "civ5-min.png" },
+	{ name: "TERA Europe", img: "tera-min.png" },
+	{ name: "Runescape", img: "runescape-min.png" },
+	{ name: "Spiral Knights", img: "spiralk-min.png" },
+	{ name: "Metin2", img: "metin2-min.png" },
+	{ name: "Teeworlds", img: "teeworlds-min.png" }
 ];
 
 for (let game of games){
@@ -46,7 +46,7 @@ for (let gamer of gamers){
 	let img = document.createElement("img");
 	let cap = document.createElement("figcaption");
 
-	img.src = "res/img/gamers/" + gamer.toLowerCase() + ".jpg";
+	img.src = "res/img/gamers/" + gamer.toLowerCase() + "-min.jpg";
 	img.alt = gamer;
 	cap.innerHTML = gamer;
 
